e121702f67dce25706a48650410e7bd9c52df9e9
Oumaima1602 <oumabi776@gmail.com>
2023-04-27 23:41:30 +0100

    adding Genre Repository/Service/Controller/Front

---
+++ src/main/java/com/hendisantika/adminlte/repository/GenreRepository.java
@@ -0,0 +1,12 @@
+package com.hendisantika.adminlte.repository;
+
+
+import com.hendisantika.adminlte.model.Genre;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GenreRepository extends JpaRepository<Genre, Long>{
+
+}
